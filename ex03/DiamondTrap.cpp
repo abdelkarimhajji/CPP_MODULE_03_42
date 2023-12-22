b856e16b708d1b2aedc80103cd3cd685cd272026
abdelkarim hajji <hajji.karim.solicode@gmail.com>
2023-12-22 09:36:03 +0100

    push this project

--- ex03/DiamondTrap.cpp
+++ ex03/DiamondTrap.cpp
@@ -6,7 +6,7 @@
 /*   By: ahajji <ahajji@student.1337.ma>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/12/20 10:38:16 by ahajji            #+#    #+#             */
-/*   Updated: 2023/12/20 11:47:01 by ahajji           ###   ########.fr       */
+/*   Updated: 2023/12/20 13:12:42 by ahajji           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,10 +15,13 @@
 DiamondTrap::DiamondTrap(std::string name) : ClapTrap(name + "_clap_name"), FragTrap(name), ScavTrap(name)
 {
     this->name = name;
+    this->hitPoints = 100;
+    this->energyPoints = 50;
+    this->attackDamage = 30;
     std::cout << "This is the constructer DiamondTrap" << std::endl;
 }
 
 DiamondTrap::~DiamondTrap()
 {
-    
+    std::cout << "This is the deconstructer DiamondTrap" << std::endl;
 }
